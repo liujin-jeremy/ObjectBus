e62bf9e7f5fea31902283bcb951a2803b393fe02
threekilogram <wuxiongc@163.com>
2018-05-03 06:48:22 +0800

    初始化项目,该库抽离于一个内部库

---
+++ app/src/main/java/com/example/wuxio/objectbus/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wuxio.objectbus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author wuxio
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
