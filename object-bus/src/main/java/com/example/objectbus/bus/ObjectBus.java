c0772f02502047807faecdf85541864f89782aee
threekilogram <wuxiongc@163.com>
2018-05-04 10:32:07 +0800

    ObjectBus 优化注释

--- object-bus/src/main/java/com/example/objectbus/bus/ObjectBus.java
+++ object-bus/src/main/java/com/example/objectbus/bus/ObjectBus.java
@@ -5,15 +5,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.util.ArrayMap;
 
 import com.example.objectbus.executor.AppExecutor;
-import com.example.objectbus.executor.OnExecuteRunnable;
 import com.example.objectbus.message.Messengers;
 import com.example.objectbus.message.OnMessageReceiveListener;
-import com.example.objectbus.schedule.Scheduler;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * 该类穿梭于线程之间,执行各种操作
+ *
  * @author wuxio 2018-05-03:16:16
  */
 public class ObjectBus {
@@ -38,7 +38,7 @@ public class ObjectBus {
     private int threadCurrent;
 
     /**
-     * record current is resting
+     * record  run State is resting
      */
     private static final int RUN_STATE_RUNNING        = 0X10EE;
     private static final int RUN_STATE_RESTING        = 0X11EE;
@@ -56,24 +56,27 @@ public class ObjectBus {
     private ArrayList< Command > mHowToPass = new ArrayList<>();
 
     /**
-     * used do runnable at {@link com.example.objectbus.executor.AppExecutor}
+     * used do runnable at {@link com.example.objectbus.executor.AppExecutor},used by
+     * {@link #COMMAND_TO_UNDER}
      */
-    private BusOnExecuteRunnable mBusOnExecuteRunnable = new BusOnExecuteRunnable();
+    private ExecutorRunnable mExecutorRunnable;
 
     /**
-     * used do runnable at MainThread
+     * used do runnable at MainThread with {@link #COMMAND_TO_MAIN},
+     * {@link #stopRest()}:to stop bus rest
+     * {@link TakeWhileRunnable#run()}: to top bus rest
      */
     private BusMessageListener mBusMessageListener = new BusMessageListener();
 
     /**
-     * take customs to bus,{@link #takeAs(Object, String)}
+     * take customs to bus,{@link #takeAs(Object, String)},{@link #get(String)},{@link #off(String)}
      */
     private ArrayMap< String, Object > mExtras = new ArrayMap<>();
 
     /**
-     * do nothing just take a rest ,wait notify
+     * do nothing just take a rest ,{@link #COMMAND_TAKE_REST}
      */
-    private RestRunnable mRestRunnable = new RestRunnable();
+    private RestRunnable mRestRunnable;
 
 
     public ObjectBus() {
@@ -82,7 +85,7 @@ public class ObjectBus {
 
 
     /**
-     * @return hoe many station pass by
+     * @return did how many task
      */
     public int getPassBy() {
 
@@ -91,7 +94,7 @@ public class ObjectBus {
 
 
     /**
-     * to next station
+     * to next station,
      */
     private void toNextStation() {
 
@@ -102,7 +105,6 @@ public class ObjectBus {
             doCommand(command);
         } else {
 
-            // TODO: 2018-05-03 how to set current value
             mPassBy.getAndAdd(-1);
         }
     }
@@ -113,56 +115,106 @@ public class ObjectBus {
      */
     private void doCommand(Command command) {
 
+        /* run runnable on current thread, current thread depends on before command run on which */
+
         if (command.command == COMMAND_GO) {
-            command.run();
+
+            Runnable runnable = command.getRunnable();
+            runnable = wrapperRunnableIfHaveOnBusRunningListener(runnable);
+            runnable.run();
+
             toNextStation();
             return;
         }
 
+        /* run runnable on threadPool */
+
         if (command.command == COMMAND_TO_UNDER) {
+
             if (threadCurrent != THREAD_EXECUTOR) {
-                mBusOnExecuteRunnable.setRunnable(command.getRunnable());
-                Scheduler.todo(mBusOnExecuteRunnable);
+
+                /* not in pool change to pool */
+
+                if (mExecutorRunnable == null) {
+                    mExecutorRunnable = new ExecutorRunnable();
+                }
+                Runnable runnable = command.getRunnable();
+                runnable = wrapperRunnableIfHaveOnBusRunningListener(runnable);
+                mExecutorRunnable.setRunnable(runnable);
+                AppExecutor.execute(mExecutorRunnable);
                 threadCurrent = THREAD_EXECUTOR;
             } else {
-                command.run();
+
+                /* still in pool, run runnable */
+
+                Runnable runnable = command.getRunnable();
+                runnable = wrapperRunnableIfHaveOnBusRunningListener(runnable);
+                runnable.run();
                 toNextStation();
             }
             return;
         }
 
+        /* run runnable on MainThread */
+
         if (command.command == COMMAND_TO_MAIN) {
+
             if (threadCurrent != THREAD_MAIN) {
+
+                /* not on main, use messenger change to MainThread */
+
                 BusMessageListener messenger = mBusMessageListener;
-                messenger.setRunnable(command.getRunnable());
+                Runnable runnable = command.getRunnable();
+                runnable = wrapperRunnableIfHaveOnBusRunningListener(runnable);
+                messenger.setRunnable(runnable);
                 messenger.runOnMain();
                 threadCurrent = THREAD_MAIN;
             } else {
-                command.run();
+
+                /* still on Main, runRunnable */
+
+                command.getRunnable().run();
                 toNextStation();
             }
 
             return;
         }
 
+        /* send a message on currentThread */
+
         if (command.command == COMMAND_SEND) {
-            command.run();
+            Runnable runnable = command.getRunnable();
+            runnable = wrapperRunnableIfHaveOnBusRunningListener(runnable);
+            runnable.run();
             toNextStation();
             return;
         }
 
+        /* bus take a rest */
+
         if (command.command == COMMAND_TAKE_REST) {
+
             runState = RUN_STATE_RESTING;
-            /*  do nothing,wait for notify to go on */
+
+            /* did'nt toNextStation(), wait util stopRest() called */
+
             return;
         }
 
+        /* bus take a while, then go on */
+
         if (command.command == COMMAND_TAKE_REST_AWHILE) {
+
+            /* just take a while, when time up toNextStation(), or stopRest() called toNextStation() */
+
             runState = RUN_STATE_RESTING_AWHILE;
-            command.run();
-            return;
+            command.getRunnable().run();
+
+            //return;
         }
     }
+
+    //============================ flow action ============================
 
 
     /**
@@ -208,7 +260,7 @@ public class ObjectBus {
 
 
     /**
-     * send message on current thread,same as {@link #go(Runnable)}
+     * send message on current thread
      *
      * @param what     message what
      * @param listener receiver
@@ -221,7 +273,7 @@ public class ObjectBus {
 
 
     /**
-     * send message on current thread,same as {@link #go(Runnable)}
+     * send message on current thread
      *
      * @param what     message what
      * @param extra    extra msg
@@ -235,7 +287,7 @@ public class ObjectBus {
 
 
     /**
-     * send message on current thread,same as {@link #go(Runnable)}
+     * send message on current thread
      *
      * @param what     message what
      * @param listener receiver
@@ -248,13 +300,14 @@ public class ObjectBus {
 
 
     /**
-     * send message on current thread,same as {@link #go(Runnable)}
+     * send message on current thread
      *
      * @param what     message what
      * @param extra    extra msg
      * @param listener receiver
      * @return self
      */
+    @SuppressWarnings("WeakerAccess")
     public ObjectBus sendDelayed(int what, int delayed, Object extra, OnMessageReceiveListener listener) {
 
         mHowToPass.add(new Command(COMMAND_SEND, new SendRunnable(what, delayed, extra, listener)));
@@ -263,15 +316,24 @@ public class ObjectBus {
 
 
     /**
-     * take a rest
+     * take a rest ,util {@link #stopRest()} called
      */
     public ObjectBus takeRest() {
 
+        if (mRestRunnable == null) {
+            mRestRunnable = new RestRunnable();
+        }
         mHowToPass.add(new Command(COMMAND_TAKE_REST, mRestRunnable));
         return this;
     }
 
 
+    /**
+     * take a rest for a while ,util time up, or {@link #stopRest()} called
+     *
+     * @param millisecond time to rest
+     * @return self
+     */
     public ObjectBus takeRest(int millisecond) {
 
         mHowToPass.add(new Command(COMMAND_TAKE_REST_AWHILE, new TakeWhileRunnable(millisecond)));
@@ -279,6 +341,9 @@ public class ObjectBus {
     }
 
 
+    /**
+     * when this called , if bus is resting , bus will go on
+     */
     public void stopRest() {
 
         if (runState == RUN_STATE_RESTING || runState == RUN_STATE_RESTING_AWHILE) {
@@ -337,6 +402,82 @@ public class ObjectBus {
         return mExtras.remove(key);
     }
 
+    //============================ bus listener ============================
+
+    private OnBusRunningListener       mOnBusRunningListener;
+    private BusRunningListenerRunnable mBusRunningListenerRunnable;
+
+
+    public void setOnBusRunningListener(OnBusRunningListener onBusRunningListener) {
+
+        mOnBusRunningListener = onBusRunningListener;
+    }
+
+
+    public OnBusRunningListener getOnBusRunningListener() {
+
+        return mOnBusRunningListener;
+    }
+
+
+    /**
+     * bus run runnable listener
+     */
+    public interface OnBusRunningListener {
+
+        /**
+         * before runnable start call
+         *
+         * @param bus      bus
+         * @param runnable to run
+         */
+        void onRunnableStart(ObjectBus bus, Runnable runnable);
+
+        /**
+         * after runnable finish
+         *
+         * @param bus      bus
+         * @param runnable finished
+         */
+        void onRunnableFinished(ObjectBus bus, Runnable runnable);
+
+        /**
+         * called when runnable exception
+         *
+         * @param bus      bus
+         * @param runnable runnable get Exception
+         * @param e        Exception
+         */
+        void onRunnableException(ObjectBus bus, Runnable runnable, Exception e);
+
+    }
+
+
+    /**
+     * if have a {@link #mOnBusRunningListener} use {@link BusRunningListenerRunnable} wrapper {@code
+     * runnable}
+     *
+     * @param runnable user's runnable
+     * @return {@link BusRunningListenerRunnable} or runnable self
+     */
+    private Runnable wrapperRunnableIfHaveOnBusRunningListener(Runnable runnable) {
+
+        if (mOnBusRunningListener != null) {
+
+            if (mBusRunningListenerRunnable == null) {
+                mBusRunningListenerRunnable = new BusRunningListenerRunnable(mOnBusRunningListener);
+            } else {
+
+                mBusRunningListenerRunnable.changeListener(mOnBusRunningListener);
+            }
+
+            mBusRunningListenerRunnable.setRunnable(runnable);
+            return mBusRunningListenerRunnable;
+        }
+
+        return runnable;
+    }
+
     //============================ command for Bus run runnable ============================
 
     /**
@@ -344,7 +485,16 @@ public class ObjectBus {
      */
     private class Command {
 
+        /**
+         * one of {@link #COMMAND_GO} {@link #COMMAND_SEND} {@link #COMMAND_TAKE_REST}
+         * {@link #COMMAND_TO_MAIN} {@link #COMMAND_TO_UNDER} {@link #COMMAND_TAKE_REST_AWHILE}
+         *
+         * bus use this command to decide what to do
+         */
         private int      command;
+        /**
+         * the runnable if the user want to do,bus will run this with command
+         */
         private Runnable mRunnable;
 
 
@@ -352,12 +502,6 @@ public class ObjectBus {
 
             this.command = command;
             mRunnable = runnable;
-        }
-
-
-        void run() {
-
-            mRunnable.run();
         }
 
 
@@ -371,8 +515,10 @@ public class ObjectBus {
 
     /**
      * use take task to do in the {@link com.example.objectbus.executor.AppExecutor}
+     *
+     * {@link #mExecutorRunnable}
      */
-    private class BusOnExecuteRunnable implements OnExecuteRunnable {
+    private class ExecutorRunnable implements Runnable {
 
         private Runnable mRunnable;
 
@@ -384,41 +530,34 @@ public class ObjectBus {
 
 
         @Override
-        public void onExecute() {
+        public void run() {
+
+            /* this will run on  AppExecutor */
 
             Runnable runnable = mRunnable;
             if (runnable != null) {
                 runnable.run();
             }
-        }
-
-
-        @Override
-        public void onFinish() {
-
             toNextStation();
-        }
-
-
-        @Override
-        public void onException(Exception e) {
-
-            // TODO: 2018-05-03 how to handle exception
-
         }
     }
 
     //============================ main runnable ============================
 
+    /**
+     * bus use this to communicate with {@link Messengers}
+     */
     private class BusMessageListener implements OnMessageReceiveListener {
 
-        private static final String TAG = "BusMessageListener";
+        //============================ to run on min ============================
 
-        private static final int WHAT_MAIN         = 3;
-        private static final int WHAT_STOP_REST    = 4;
-        private static final int WHAT_NEXT_AT_MAIN = 5;
-        private static final int WHAT_REST_TIME_UP = 6;
-
+        /**
+         * when receive this msg, take bus to Main Thread to run
+         */
+        private static final int WHAT_MAIN = 3;
+        /**
+         * used with {@link #WHAT_MAIN},when receive {@link #WHAT_MAIN},run this runnable on mainThread
+         */
         private Runnable mRunnable;
 
 
@@ -436,9 +575,25 @@ public class ObjectBus {
             Messengers.send(WHAT_MAIN, this);
         }
 
+        //============================ bus stop rest ============================
 
         /**
-         * notify bus to next station when bus is RESTING
+         * when receive this msg take bus to last Command run Thread to go on
+         */
+        private static final int WHAT_STOP_REST    = 4;
+        /**
+         * when receive this msg means bus last command run on MainThread,to MainThread go on
+         */
+        private static final int WHAT_NEXT_AT_MAIN = 5;
+        /**
+         * when receive this msg means bus rest time up, need go on
+         */
+        private static final int WHAT_REST_TIME_UP = 6;
+
+
+        /**
+         * notify bus to next station if bus is RESTING, called from {@link #stopRest()},
+         * when this called bus must in resting
          */
         void notifyBusStopRest() {
 
@@ -461,6 +616,8 @@ public class ObjectBus {
             Messengers.send(WHAT_NEXT_AT_MAIN, this);
         }
 
+        //============================ actual action ============================
+
 
         @Override
         public void onReceive(int what) {
@@ -470,8 +627,8 @@ public class ObjectBus {
             if (what == WHAT_MAIN) {
                 if (mRunnable != null) {
                     mRunnable.run();
-                    toNextStation();
                 }
+                toNextStation();
                 return;
             }
 
@@ -481,9 +638,13 @@ public class ObjectBus {
 
                 /* base on bus threadCurrent when him rest,to go on at that thread */
 
+                /* this is running at Messengers#Thread not in threadPool or MainThread*/
+
                 if (threadCurrent == THREAD_MAIN) {
+
                     takeBusAtMainToNext();
                 } else {
+
                     AppExecutor.execute(ObjectBus.this::toNextStation);
                 }
 
@@ -497,6 +658,8 @@ public class ObjectBus {
                 return;
             }
 
+            /* time up to notify bus to go on */
+
             if (what == WHAT_REST_TIME_UP) {
 
                 if (runState == RUN_STATE_RESTING_AWHILE) {
@@ -508,6 +671,12 @@ public class ObjectBus {
 
     //============================ Send runnable ============================
 
+    /**
+     * used to send Message with {@link #COMMAND_SEND}
+     * {@link #sendDelayed(int, int, Object, OnMessageReceiveListener)}
+     *
+     * @see Messengers#send(int, int, Object, OnMessageReceiveListener)
+     */
     private class SendRunnable implements Runnable {
 
         private int                      what;
@@ -528,6 +697,8 @@ public class ObjectBus {
         @Override
         public void run() {
 
+            /* send message */
+
             if (extra == null) {
 
                 Messengers.send(what, delayed, receiveListener);
@@ -536,10 +707,38 @@ public class ObjectBus {
                 Messengers.send(what, delayed, extra, receiveListener);
             }
         }
+
+
+        public int getWhat() {
+
+            return what;
+        }
+
+
+        public int getDelayed() {
+
+            return delayed;
+        }
+
+
+        public Object getExtra() {
+
+            return extra;
+        }
+
+
+        public OnMessageReceiveListener getReceiveListener() {
+
+            return receiveListener;
+        }
     }
 
     //============================ rest Runnable ============================
 
+    /**
+     * used with {@link #COMMAND_TAKE_REST},
+     * because {@link Command#Command(int, Runnable)} not null,so create a do Nothing runnable
+     */
     private class RestRunnable implements Runnable {
 
         @Override
@@ -552,6 +751,10 @@ public class ObjectBus {
 
     //============================ take a while Runnable ============================
 
+    /**
+     * used with {@link #COMMAND_TAKE_REST_AWHILE} ,
+     * send a delayed message, to call {@link #stopRest()}
+     */
     private class TakeWhileRunnable implements Runnable {
 
         private int delayed;
@@ -567,6 +770,63 @@ public class ObjectBus {
         public void run() {
 
             mBusMessageListener.notifyBusStopRestAfter(delayed);
+        }
+    }
+
+    //============================ BusRunningListenerRunnable ============================
+
+    /**
+     * when user {@link #setOnBusRunningListener(OnBusRunningListener)},use this to
+     * {@link #wrapperRunnableIfHaveOnBusRunningListener(Runnable)} the user runnable to listen
+     */
+    private class BusRunningListenerRunnable implements Runnable {
+
+        /**
+         * {@link #setOnBusRunningListener(OnBusRunningListener)}
+         */
+        OnBusRunningListener mOnBusRunningListener;
+        /**
+         * {@link Command#mRunnable}
+         */
+        Runnable             mRunnable;
+
+
+        BusRunningListenerRunnable(OnBusRunningListener onBusRunningListener) {
+
+            mOnBusRunningListener = onBusRunningListener;
+        }
+
+
+        void changeListener(OnBusRunningListener onBusRunningListener) {
+
+            mOnBusRunningListener = onBusRunningListener;
+        }
+
+
+        void setRunnable(Runnable runnable) {
+
+            mRunnable = runnable;
+        }
+
+
+        @Override
+        public void run() {
+
+            /* run the runnable user set with the OnBusRunningListener */
+
+            Runnable runnable = mRunnable;
+            OnBusRunningListener listener = mOnBusRunningListener;
+            listener.onRunnableStart(ObjectBus.this, runnable);
+
+            try {
+
+                runnable.run();
+                listener.onRunnableFinished(ObjectBus.this, runnable);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                listener.onRunnableException(ObjectBus.this, runnable, e);
+            }
         }
     }
 }
