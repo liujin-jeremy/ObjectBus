2bc0ff51d8f68c8cccd4f83317af06a3d490059e
liujin <wuxiongc@163.com>
2018-09-11 17:05:37 +0800

    update echo

--- object-bus/src/main/java/com/threekilogram/objectbus/runnable/BaseEchoRunnable.java
+++ object-bus/src/main/java/com/threekilogram/objectbus/runnable/BaseEchoRunnable.java
@@ -31,6 +31,17 @@ public abstract class BaseEchoRunnable<V> implements Runnable {
       }
 
       /**
+       * 当任务处理完毕之后可以调用该方法,设置结果,结果将会被传送到主线程{@link #onResult(Object)}方法
+       *
+       * @param runnable 设置结束任务
+       */
+      @SuppressWarnings("WeakerAccess")
+      protected void setResult ( Runnable runnable ) {
+
+            MainExecutor.execute( runnable );
+      }
+
+      /**
        * 主线程接收结果
        *
        * @param result 结果{@link #setResult(Object)}
